0b6e5e471fb9eea8afaf4fd27f59ec7dcd417a78
connor steele <connorsteele604@gmail.com>
2015-11-30 22:18:56 +0000

    added better result

--- application.js
+++ application.js
@@ -1,7 +1,7 @@
 function arrayOfLight(num){
   var array = [];
   for(var i = 0; i <= num; i++){
-    array[i] = i
+    array.push(i) //changed to be more precise, old answer only worked because we were starting from 0.
   }
   return array;
 }
